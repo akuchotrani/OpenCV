3a579b2673dd648f8652598b454be0221188af41
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2018-02-26 19:23:29 -0800

    rendering gray frame

--- OpenCvVideo.py
+++ OpenCvVideo.py
@@ -19,7 +19,8 @@ while (cap.isOpened()):
     
     if ret == True:
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame',gray)
+        cv2.imshow('frame2',gray)
+        cv2.imshow('frame',frame)
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
